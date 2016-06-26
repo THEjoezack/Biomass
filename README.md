52bc879fa7f9c31909d387d4f3503b35a90a1351
THEjoezack <me@joezack.com>
2016-06-26 15:16:52 -0400

    Initial

---
+++ README.md
@@ -0,0 +1,8 @@
+mkdir RogueOS;
+cd RogueOS;
+npm init
+npm install --save rot-js
+npm install --save-dev gulp
+
+// Gulp Dependencies
+var gulp = require('gulp');
---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var bower = require('gulp-bower');
+ 
+gulp.task('bower', function() {
+  return bower();
+});
+
+gulp.task('lint', function() {
+  return gulp.src('./src/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
